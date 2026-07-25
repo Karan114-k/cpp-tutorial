2013102314225318310715bb04adf51d834e3277
Karan <karan8kumar12@gmail.com>
2026-07-25 08:14:18 +0530

    Add dataType.cpp to demonstrate variable sizes

---
+++ dataType.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    string name;
+    float b;
+    bool c;
+    double e;
+    char d;
+    cout << sizeof(a) << endl;
+    cout << sizeof(name) << endl;
+    cout << sizeof(b) << endl;
+    cout << sizeof(c) << endl;
+    cout << sizeof(e) << endl;
+    cout << sizeof(d) << endl;
+}
